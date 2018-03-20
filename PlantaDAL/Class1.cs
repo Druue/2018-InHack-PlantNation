981e2845b2a85eaa1212f7da3998d087f83ee080
Plez <615284@student.inholland.nl>
2018-03-20 18:20:59 +0100

    References complete

---
+++ PlantaDAL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantaDAL
+{
+    public class Class1
+    {
+    }
+}
