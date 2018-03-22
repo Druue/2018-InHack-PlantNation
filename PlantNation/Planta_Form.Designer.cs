05f054c385af494658f6de1953d676b8d6093d0c
KimHouweling <550178@student.inholland.nl>
2018-03-22 09:33:18 +0100

    Windows basic form

--- PlantNation/Planta_Form.Designer.cs
+++ PlantNation/Planta_Form.Designer.cs
@@ -28,12 +28,251 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Panel_menu = new System.Windows.Forms.Panel();
+            this.Panel_header = new System.Windows.Forms.Panel();
+            this.Panel_content = new System.Windows.Forms.Panel();
+            this.lbl_Dashboard = new System.Windows.Forms.Label();
+            this.lbl_add = new System.Windows.Forms.Label();
+            this.lbl_change = new System.Windows.Forms.Label();
+            this.lbl_delete = new System.Windows.Forms.Label();
+            this.lbl_journal = new System.Windows.Forms.Label();
+            this.lbl_ranks = new System.Windows.Forms.Label();
+            this.lbl_about = new System.Windows.Forms.Label();
+            this.lbl_username = new System.Windows.Forms.Label();
+            this.lbl_currentTab = new System.Windows.Forms.Label();
+            this.pictureBox_Planta = new System.Windows.Forms.PictureBox();
+            this.lbl_close = new System.Windows.Forms.Label();
+            this.lbl_minimize = new System.Windows.Forms.Label();
+            this.Panel_menu.SuspendLayout();
+            this.Panel_header.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Planta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Panel_menu
+            // 
+            this.Panel_menu.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.Panel_menu.BackColor = System.Drawing.Color.Transparent;
+            this.Panel_menu.Controls.Add(this.lbl_about);
+            this.Panel_menu.Controls.Add(this.lbl_ranks);
+            this.Panel_menu.Controls.Add(this.lbl_journal);
+            this.Panel_menu.Controls.Add(this.lbl_delete);
+            this.Panel_menu.Controls.Add(this.lbl_change);
+            this.Panel_menu.Controls.Add(this.lbl_add);
+            this.Panel_menu.Controls.Add(this.lbl_Dashboard);
+            this.Panel_menu.Location = new System.Drawing.Point(0, 119);
+            this.Panel_menu.Name = "Panel_menu";
+            this.Panel_menu.Size = new System.Drawing.Size(200, 447);
+            this.Panel_menu.TabIndex = 0;
+            // 
+            // Panel_header
+            // 
+            this.Panel_header.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Panel_header.BackColor = System.Drawing.Color.Transparent;
+            this.Panel_header.Controls.Add(this.lbl_minimize);
+            this.Panel_header.Controls.Add(this.lbl_currentTab);
+            this.Panel_header.Controls.Add(this.lbl_username);
+            this.Panel_header.Location = new System.Drawing.Point(206, 12);
+            this.Panel_header.Name = "Panel_header";
+            this.Panel_header.Size = new System.Drawing.Size(842, 100);
+            this.Panel_header.TabIndex = 1;
+            // 
+            // Panel_content
+            // 
+            this.Panel_content.BackColor = System.Drawing.Color.Transparent;
+            this.Panel_content.Location = new System.Drawing.Point(207, 119);
+            this.Panel_content.Name = "Panel_content";
+            this.Panel_content.Size = new System.Drawing.Size(841, 447);
+            this.Panel_content.TabIndex = 2;
+            // 
+            // lbl_Dashboard
+            // 
+            this.lbl_Dashboard.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbl_Dashboard.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Dashboard.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_Dashboard.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Dashboard.Location = new System.Drawing.Point(12, 59);
+            this.lbl_Dashboard.Name = "lbl_Dashboard";
+            this.lbl_Dashboard.Size = new System.Drawing.Size(173, 43);
+            this.lbl_Dashboard.TabIndex = 3;
+            this.lbl_Dashboard.Text = "Dashboard";
+            this.lbl_Dashboard.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_add
+            // 
+            this.lbl_add.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbl_add.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_add.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_add.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_add.Location = new System.Drawing.Point(12, 110);
+            this.lbl_add.Name = "lbl_add";
+            this.lbl_add.Size = new System.Drawing.Size(173, 43);
+            this.lbl_add.TabIndex = 4;
+            this.lbl_add.Text = "Add plant";
+            this.lbl_add.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_change
+            // 
+            this.lbl_change.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbl_change.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_change.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_change.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_change.Location = new System.Drawing.Point(12, 161);
+            this.lbl_change.Name = "lbl_change";
+            this.lbl_change.Size = new System.Drawing.Size(173, 43);
+            this.lbl_change.TabIndex = 5;
+            this.lbl_change.Text = "Change plant";
+            this.lbl_change.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_delete
+            // 
+            this.lbl_delete.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbl_delete.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_delete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_delete.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_delete.Location = new System.Drawing.Point(12, 212);
+            this.lbl_delete.Name = "lbl_delete";
+            this.lbl_delete.Size = new System.Drawing.Size(173, 43);
+            this.lbl_delete.TabIndex = 6;
+            this.lbl_delete.Text = "Delete plant";
+            this.lbl_delete.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_journal
+            // 
+            this.lbl_journal.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbl_journal.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_journal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_journal.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_journal.Location = new System.Drawing.Point(12, 263);
+            this.lbl_journal.Name = "lbl_journal";
+            this.lbl_journal.Size = new System.Drawing.Size(173, 43);
+            this.lbl_journal.TabIndex = 7;
+            this.lbl_journal.Text = "Ranks";
+            this.lbl_journal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_ranks
+            // 
+            this.lbl_ranks.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbl_ranks.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_ranks.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_ranks.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ranks.Location = new System.Drawing.Point(12, 314);
+            this.lbl_ranks.Name = "lbl_ranks";
+            this.lbl_ranks.Size = new System.Drawing.Size(173, 43);
+            this.lbl_ranks.TabIndex = 8;
+            this.lbl_ranks.Text = "Journal";
+            this.lbl_ranks.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_about
+            // 
+            this.lbl_about.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.lbl_about.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_about.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_about.Font = new System.Drawing.Font("Segoe UI Emoji", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_about.Location = new System.Drawing.Point(12, 365);
+            this.lbl_about.Name = "lbl_about";
+            this.lbl_about.Size = new System.Drawing.Size(173, 43);
+            this.lbl_about.TabIndex = 9;
+            this.lbl_about.Text = "About";
+            this.lbl_about.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Location = new System.Drawing.Point(569, 13);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(121, 17);
+            this.lbl_username.TabIndex = 0;
+            this.lbl_username.Text = "Display username";
+            this.lbl_username.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_currentTab
+            // 
+            this.lbl_currentTab.AutoSize = true;
+            this.lbl_currentTab.Font = new System.Drawing.Font("Segoe UI Emoji", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_currentTab.Location = new System.Drawing.Point(13, 29);
+            this.lbl_currentTab.Name = "lbl_currentTab";
+            this.lbl_currentTab.Size = new System.Drawing.Size(246, 46);
+            this.lbl_currentTab.TabIndex = 1;
+            this.lbl_currentTab.Text = "exp Dashboard";
+            // 
+            // pictureBox_Planta
+            // 
+            this.pictureBox_Planta.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox_Planta.Name = "pictureBox_Planta";
+            this.pictureBox_Planta.Size = new System.Drawing.Size(200, 110);
+            this.pictureBox_Planta.TabIndex = 3;
+            this.pictureBox_Planta.TabStop = false;
+            // 
+            // lbl_close
+            // 
+            this.lbl_close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_close.AutoSize = true;
+            this.lbl_close.BackColor = System.Drawing.Color.Red;
+            this.lbl_close.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_close.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_close.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.lbl_close.Location = new System.Drawing.Point(1029, 12);
+            this.lbl_close.Name = "lbl_close";
+            this.lbl_close.Size = new System.Drawing.Size(19, 19);
+            this.lbl_close.TabIndex = 2;
+            this.lbl_close.Text = "X";
+            this.lbl_close.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // lbl_minimize
+            // 
+            this.lbl_minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_minimize.AutoSize = true;
+            this.lbl_minimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lbl_minimize.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_minimize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lbl_minimize.Location = new System.Drawing.Point(799, 0);
+            this.lbl_minimize.Name = "lbl_minimize";
+            this.lbl_minimize.Size = new System.Drawing.Size(18, 19);
+            this.lbl_minimize.TabIndex = 2;
+            this.lbl_minimize.Text = "_";
+            // 
+            // Planta_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1060, 578);
+            this.Controls.Add(this.lbl_close);
+            this.Controls.Add(this.pictureBox_Planta);
+            this.Controls.Add(this.Panel_content);
+            this.Controls.Add(this.Panel_header);
+            this.Controls.Add(this.Panel_menu);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1078, 625);
+            this.Name = "Planta_Form";
+            this.Text = "Planta";
+            this.Panel_menu.ResumeLayout(false);
+            this.Panel_header.ResumeLayout(false);
+            this.Panel_header.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_Planta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel Panel_menu;
+        private System.Windows.Forms.Label lbl_about;
+        private System.Windows.Forms.Label lbl_ranks;
+        private System.Windows.Forms.Label lbl_journal;
+        private System.Windows.Forms.Label lbl_delete;
+        private System.Windows.Forms.Label lbl_change;
+        private System.Windows.Forms.Label lbl_add;
+        private System.Windows.Forms.Label lbl_Dashboard;
+        private System.Windows.Forms.Panel Panel_header;
+        private System.Windows.Forms.Panel Panel_content;
+        private System.Windows.Forms.Label lbl_minimize;
+        private System.Windows.Forms.Label lbl_currentTab;
+        private System.Windows.Forms.Label lbl_username;
+        private System.Windows.Forms.PictureBox pictureBox_Planta;
+        private System.Windows.Forms.Label lbl_close;
     }
 }
 
