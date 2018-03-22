ddea7c11182f4039aa64f6644450d4d7f5edfde0
Sophie Ibbs <622294@student.inholland.nl>
2018-03-22 11:19:57 +0100

    Made isDead stuff

--- PlantaLogic/PlantaLogic.cs
+++ PlantaLogic/PlantaLogic.cs
@@ -9,7 +9,95 @@ using PlantaModel;
 /// </summary>
 namespace PlantaLogic
 {
-    public class PlantaLogic
+    class PlantaLogic
     {
+        public class isDead
+        {
+            bool[] isdead = new bool[5];
+            int last_watered;
+            int dead_count = 0;
+
+            //the points system
+            int points = 0;
+            int points_watered = 50;
+            int points_missed = -25;
+            int points_death = -100;
+
+            public bool deadPlants()
+            {
+                for (int i = 0; i < isdead.Length; i++)
+                {
+
+                    string[] split = isdead[i].Split("€");
+                    int watering_interval = Int32.Parse(split[2]);
+
+                    //short interval plants
+                    if (watering_interval <= 4)
+                    {
+                        isdead[i] = shortInterval(last_watered, watering_interval);
+
+                        if (isdead)
+                        {
+                            dead_count++;
+                            points += points_death;
+                        }
+                    }
+                    //long interval plants
+                    else
+                    {
+                        isdead[i] = longInterval(last_watered, watering_interval);
+                        if (isdead)
+                        {
+
+                            dead_count++;
+                            points += points_death;
+
+                        }
+                    }
+                }
+            }
+
+            public bool shortInterval(int last_watered, int interval)
+            {
+                //maximum days without watering
+                int max_days = interval * 4;
+
+                //determining the time
+                DateTime today = DateTime.Today;
+                int time_lastWater = today - last_watered;
+
+                //dead
+                if (time_lastWater > max_days)
+                {
+                    return true;
+                }
+                //alive
+                else
+                {
+                    return false;
+                }
+            }
+
+            public bool longInterval(int last_watered, int interval)
+            {
+                //maximum days without watering
+                int max_days = interval * 2;
+
+                //determining the time
+                DateTime today = DateTime.Today;
+                int time_lastWater = today - last_watered;
+
+                //dead
+                if (time_lastWater > max_days)
+                {
+                    return true;
+                }
+                //alive
+                else
+                {
+                    return false;
+                }
+            }
+        }
     }
 }
